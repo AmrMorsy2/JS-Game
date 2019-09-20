161041691c30595b9e7ed5d0d3c48a61b300526f
Amr Morsy <amrmorsy2@gmail.com>
2019-09-20 07:53:20 +0200

    Added Change Direction Function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 var body = document.getElementsByTagName("body");
 let coin = document.createElement("img");
-coin.setAttribute("src","assets/myCoin.jpg" );
+coin.setAttribute("src", "assets/myCoin.jpg");
 coin.style.width = "50px"
 coin.style.height = "50px"
-var renderElement= function(x,y,img){
-        img.style.top = x;
-        img.style.bottom = y;
+var renderElement = function (x, y, img) {
+        img.style.position = "absolute";
+        img.style.top = x + "px";
+        img.style.left = y + "px";
         body[0].appendChild(img);
-} 
+}
 
- renderElement("200px","-200px",coin)
+renderElement(200, 200, coin)
 console.log(coin)
+
+let dir = 0;
+
+function changeDir(evn) {
+        if (evn.key == "ArrowUp")
+                dir = 0;
+        if (evn.key == "ArrowDown")
+                dir = 1;
+        if (evn.key == "ArrowLeft")
+                dir = 2;
+        if (evn.key == "ArrowRight")
+                dir = 3;
+}
+/*
+setInterval(()=>{
+        console.log(dir);
+},500);
+*/
+document.addEventListener("keydown", changeDir);
+
