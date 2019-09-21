7da819c96f65d7c9b56eddcc6f3d6152f29d609b
engmohammedelkasrawy <eng.mohammed.mamdouh193@gmail.com>
2019-09-21 17:08:00 +0200

    tis is the two function one can render the elements and one can create anew coin evry 10 sec

---
+++ js/objects/coin.js
@@ -0,0 +1,9 @@
+export default class Coin{
+    height = "50px";
+    width = "50px";
+    img = "assets/myCoin.png"
+    constructor(height, width, x, y){
+        this.x = x
+        this.y = y;
+    }
+}
