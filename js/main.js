ae4fb35250e4419b1a9ea256be2e0ac45e04cb18
engmohammedelkasrawy <eng.mohammed.mamdouh193@gmail.com>
2019-09-22 12:33:22 +0100

    Tiles added with collision

--- js/main.js
+++ js/main.js
@@ -17,23 +17,33 @@ class Character{
                 this.y = y;
         }
 }
+class Tile {
+        height = "50px";
+        width = "50px";
+        img = "assets/tile.jpg"
+        constructor(x, y) {
+                this.x = x
+                this.y = y;
+        }
+    }
 ///////////////////////create objects//////////////
 var body = document.getElementsByTagName("body");
 var myCharacter = new Character("500px","500px");
-var coin1 = new Coin("320px","300px")
-var coin2 = new Coin("380px","450px")
-var coin3 = new Coin("500px","200px")
-var coin4 = new Coin("400px","150px")
+var tile1 = new Tile("320px","300px")
+var tile2 = new Tile("380px","450px")
+var tile3 = new Tile("500px","200px")
+var tile4 = new Tile("400px","150px")
 var coinElements = [];
+var tilesElements = [];
 var dir = 0;
-var coins  = [coin1,coin2,coin3,coin4];
+var score = 0 ;
+var tiles  = [tile1,tile2,tile3,tile4];
 //////////////////////render element function///////////////////////
 var renderElement= function(obj){
         let element = document.createElement("img");
         element.setAttribute("src",obj.img );
         element.style.width = obj.width
         element.style.height = obj.height
-
         element.style.position = "absolute"
         element.style.top = obj.x;
         element.style.left = obj.y;
@@ -59,14 +69,33 @@ var move = function(obj)
                 obj.style.left = (posX-20) +"px";
         if(dir==3)
                 obj.style.top = (posY+20) +"px";
-        if(checkCollide(obj))
-                console.log("Collide");
+        checkCollideCoin(obj);
+        checkCollideTile(obj);
+
 }
 
-function checkCollide(obj){
+function checkCollideCoin(obj){
         for(let i=0;i<coinElements.length;i++)
                 if(isCollide(coinElements[i],obj)){
                         coinElements[i].parentElement.removeChild(coinElements[i]);
+                        score++;
+                        
+if(score == 10)
+        for(let i = 0;i<tiles.length;i++){
+                var newTile = new Tile(Math.floor((Math.random() * 1000) + 1) + "px", Math.floor((Math.random() * 1000) + 1) + "px")
+                tilesElements.push(renderElement(newTile))
+        }
+
+                        console.log(score);
+                        return true;
+                }
+        return false;
+}
+function checkCollideTile(obj){
+        for(let i=0;i<tilesElements.length;i++)
+                if(isCollide(tilesElements[i],obj)){
+                        clearInterval(moveInterval);
+                        alert("game over");
                         return true;
                 }
         return false;
@@ -79,9 +108,9 @@ var createCoin = function () {
         setInterval(function () {
                 var newCoin = new Coin(Math.floor((Math.random() * 1000) + 1) + "px", Math.floor((Math.random() * 1000) + 1) + "px")
                 console.log(newCoin);
-                coins.push(newCoin);
+              //  coins.push(newCoin);
                 coinElements.push(renderElement(newCoin));
-        }, 6000)
+        }, 2000)
 }
 
 
@@ -118,4 +147,4 @@ function isCollide(obj1, obj2) {
 var moveCharacter = renderElement(myCharacter);
 setInterval(ChangeDir,100);
 createCoin();
-setInterval(move,200,moveCharacter)
+var moveInterval = setInterval(move,100,moveCharacter)
