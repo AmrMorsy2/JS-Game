e62608e3bcd874d977f2de55cf3c99fd80faf0e6
M-Bohram <mostafabohram@gmail.com>
2019-09-23 17:15:12 +0200

    master clean

--- js/objects/coin.js
+++
@@ -1,9 +0,0 @@
-export default class Coin{
-    height = "50px";
-    width = "50px";
-    img = "assets/myCoin.png"
-    constructor(height, width, x, y){
-        this.x = x
-        this.y = y;
-    }
-}
