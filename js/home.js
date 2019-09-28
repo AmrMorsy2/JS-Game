b08818e9611a8d3fe580d61017b3da31af8dfd53
AmrMorsy2 <amrmorsy2@gmail.com>
2019-09-28 02:23:31 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -5,12 +5,12 @@ var cat = document.getElementById("cat")
 
 document.getElementById("startbtn").addEventListener("click",function(){
     if(rat.checked) 
-    {localStorage.setItem("home",1)
+    {localStorage.setItem("index",1)
     console.log(localStorage.getItem("home"))
     window.location.replace("home.html");    
     }
     if(cat.checked) 
-    {localStorage.setItem("home",0)
+    {localStorage.setItem("index",0)
     console.log(localStorage.getItem("home"))
     window.location.replace("home.html");
     }
